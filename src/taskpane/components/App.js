6e05122736f808bf230e2306315067c0f886c4d8
Aashutosh Rathi <aashutoshrathi@gmail.com>
2021-10-16 00:59:18 +0530

    :rocket: feat: Basic Functions done

--- src/taskpane/components/App.js
+++ src/taskpane/components/App.js
@@ -1,19 +1,23 @@
 import * as React from "react";
 import PropTypes from "prop-types";
-import { DefaultButton } from "@fluentui/react";
+import { ChoiceGroup, DefaultButton, PrimaryButton, Stack } from "@fluentui/react";
 import Progress from "./Progress";
 // images references in the manifest
 import "../../../assets/icon-16.png";
 import "../../../assets/icon-32.png";
 import "../../../assets/icon-80.png";
 /* global Office, require */
-
+const size = 42;
+const logo = require("./../../../assets/logo-filled.png");
 const App = (props) => {
   const { title, isOfficeInitialized } = props;
 
   const [content, setContent] = React.useState();
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [options, setOptions] = React.useState([]);
+  const [selected, setSelected] = React.useState("0");
 
-  const click = async () => {
+  React.useEffect(() => {
     Office.context.mailbox.item.body.getAsync(
       "text",
       { asyncContext: "This is passed to the callback" },
@@ -21,25 +25,69 @@ const App = (props) => {
         setContent(result.value);
       }
     );
+  }, []);
+
+  React.useEffect(() => {
+    setIsLoading(true);
+    if (content) {
+      setOptions([
+        { key: "0", text: content?.toLowerCase().slice(0, 30) },
+        { key: "1", text: content?.toUpperCase().slice(0, 30) },
+        { key: "2", text: content?.slice(0, 30) },
+      ]);
+    }
+    setIsLoading(false);
+  }, [content]);
+
+  const refreshReplies = async () => {};
+
+  const reply = () => {
+    Office.context.mailbox.item.displayReplyForm(options.find((o) => o.key === selected)?.text);
   };
 
-  if (!isOfficeInitialized) {
-    return (
-      <Progress
-        title={title}
-        logo={require("./../../../assets/logo-filled.png")}
-        message="Please sideload your addin to see app body."
-      />
-    );
+  if (!isOfficeInitialized || isLoading) {
+    return <Progress title={title} logo={logo} message="Please sideload your addin to see app body." />;
   }
 
   return (
-    <div className="ms-welcome">
-      <p className="ms-font-l">Subject: {Office.context.mailbox.item.subject}</p>
-      {content && <p className="ms-font-l">Content: {content}</p>}
-      <DefaultButton className="ms-welcome__action" onClick={click}>
-        See Content
-      </DefaultButton>
+    <div>
+      <Stack
+        horizontal
+        tokens={{
+          childrenGap: "5%",
+          padding: "s1 5%",
+        }}
+      >
+        <img width={size} height={size} src={logo} alt={title} title={title} />
+        <h3>ReplAI</h3>
+      </Stack>
+      <br />
+      {content && (
+        <>
+          <ChoiceGroup
+            selectedKey={selected}
+            options={options}
+            onChange={(_e, o) => setSelected(o.key)}
+            label="We have generated few reply suggestions, Select the most suitable one"
+            required={true}
+          />
+          <br />
+        </>
+      )}
+      <Stack
+        horizontal
+        tokens={{
+          childrenGap: "4%",
+          padding: "s1 4%",
+        }}
+      >
+        <PrimaryButton onClick={reply} iconProps={{ iconName: "reply" }}>
+          Reply
+        </PrimaryButton>
+        <DefaultButton onClick={refreshReplies} iconProps={{ iconName: "refresh" }}>
+          Reset Replies
+        </DefaultButton>
+      </Stack>
     </div>
   );
 };
